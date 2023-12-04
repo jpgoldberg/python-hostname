236782022a4d516c7b1d4a7735740f50abbf3fa2
Jeffrey Goldberg <jeff@agilebits.com>
2023-12-03 18:49:39 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# `is_hostname()` for python
+
+Much to my surprise, or perhaps simply failure to search properly, 
+there is no RFC compliant python tool for that syntactically validates hostnames.
+Note that not all valid domain names are valid hostnames.
+
+To validated domain names, I recommend dnspython, and to validate Urls, I recommend pydantic. But neither offers direct validation of hostnames.
+
+## "Host" v "hostname"
+
+A host for many internet protocols can be given in the form of a domain name, an IPv4 address, or an IPv6 address. I will use the term "hostname" to require to the domain name form.
+A hostname must be a valid domain name, but not all valid domain names are hostnames.
+
+
+
+
---
+++ hostname.py
@@ -0,0 +1,61 @@
+from typing import TypeGuard, Any, Self
+import re
+import dns.name
+
+
+class Hostname:
+    @classmethod
+    def is_hostname(cls, s: Any) -> TypeGuard[Self]:
+        if not isinstance(s, (str, bytes)):
+            return False
+
+        # checks DNS requirements and converts to punycode if needed.
+        try:
+            dname: dns.name.Name = dns.name.from_text(s).canonicalize()
+        except Exception:
+            return False
+        labels = dname.to_text().split(".")
+
+        # remove root (most significant) label
+        labels = labels[:-1]
+
+        # Reject empty hostname
+        if len(labels) == 0:
+            return False
+
+        # A faster implementation would be to inline this
+        # and to avoid regular expressions by following
+        # the example in
+        #    https://github.com/rthalley/dnspython/issues/1019#issuecomment-1837247696
+
+        if not all([cls._is_label(label) for label in labels]):
+            return False
+
+        # Last (most significant) label cannot be all digits
+        if re.match(r"^\d+$", labels[-1]):
+            return False
+
+        return True
+
+    @staticmethod
+    def _is_label(s: str) -> bool:
+        """For a valid dns label, s, is valid hostname label"""
+
+        # Valid dns labels are already ASCII and mean length
+        # requirements.
+        #
+        # Labels are composed of letters, digits and hyphens
+        # A hyphen cannot be either the first or the last
+        # character.
+        # Note that because labels can be a single character
+        # long, a single regular expression for this neither
+        # simple nor human readable. So best to use separate
+        # checks
+
+        if not re.match(r"^[a-z\d-]$", s):
+            return False
+
+        if s[0] == "-" or s[-1] == "-":
+            return False
+
+        return True
