13fd273b02d624d8866673f27ea484284208d597
Jeffrey Goldberg <jeff@agilebits.com>
2023-12-04 16:38:30 -0600

    also linted

--- test_hostname.py
+++ test_hostname.py
@@ -5,16 +5,15 @@ import hostname
 
 
 class TestHostname(unittest.TestCase):
-
     TestString: TypeAlias = tuple[str, bool, str]
 
     test_strings: list[TestString] = [
         ("a.good.example", True, "simple"),
-        ("-initial.hyphen.example", False, "leading hyphen")
+        ("-initial.hyphen.example", False, "leading hyphen"),
     ]
 
     def test_is_hostname(self) -> None:
-        for (data, expected, desc) in self.test_strings:
+        for data, expected, desc in self.test_strings:
             with self.subTest(msg=desc):
                 result = hostname.Hostname.is_hostname(data)
                 self.assertEqual(result, expected)
