a6da99e7d1d2d0b3dacf397ea844d1b2a4f0ae0b
Jeffrey Goldberg <jeff@agilebits.com>
2023-12-04 23:36:06 -0600

    more tests

--- tests/test_hostname.py
+++ tests/test_hostname.py
@@ -10,6 +10,12 @@ class TestHostname(unittest.TestCase):
     test_strings: ClassVar[list[TestString]] = [
         ("a.good.example", True, "simple"),
         ("-initial.hyphen.example", False, "leading hyphen"),
+        ("szárba.szökik.hu", True, "idna"),
+        ("no..empty.labels", False, "empty label"),
+        ("123.456.78a", True, "digit labels ok"),
+        ("last.digits.123", False, "last label digits"),
+        ("under_score.in.host", False, "Controversial: no underscore at all"),
+        ("underscore.in.net_work", False, "not allowed in network names"),
     ]
 
     def test_is_hostname(self) -> None:
